afc0475ad5f6f72a2de218012b89915c66928c78
Sarmin <iamsarminkhatun@gmail.com>
2024-07-11 17:15:24 +0530

    api call update

--- src/App.js
+++ src/App.js
@@ -1,72 +1,29 @@
-// import React, { useState } from "react";
-// import "./App.css";
-// import Navbar from "./components/Navbar/navbar";
-// import Section from "./components/Sections/section";
-// import DisplayResult from "./components/Display/displayResult";
-
-// function App() {
-//   const [message, setMessage] = useState("");
-
-//   const handleSearchClick = (sectionId, featureId) => {
-//     setMessage(
-//       `Search feature in Feature ${featureId} of Section ${sectionId}`
-//     );
-//   };
-
-//   const handleAddClick = (sectionId, featureId) => {
-//     setMessage(`Add feature in Feature ${featureId} of Section ${sectionId}`);
-//   };
-
-//   return (
-//     <div className="app">
-//       <div className="navbar-container">
-//         <Navbar />
-//       </div>
-//       <div className="app-container">
-//         <div className="card-container">
-//           <Section
-//             sectionId={"A"}
-//             featureId={"1"}
-//             onSearchClick={handleSearchClick}
-//             onAddClick={handleAddClick}
-//           />
-//           <Section
-//             sectionId={"B"}
-//             featureId={"2"}
-//             onSearchClick={handleSearchClick}
-//             onAddClick={handleAddClick}
-//           />
-//           <Section
-//             sectionId={"C"}
-//             featureId={"3"}
-//             onSearchClick={handleSearchClick}
-//             onAddClick={handleAddClick}
-//           />
-//         </div>
-//         <div className="feature-container">
-//           <DisplayResult message={message} />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/navbar";
 import Section from "./components/Sections/section";
 import DisplayResult from "./components/Display/displayResult";
 
-const sectionsData = [
-  { sectionId: "A", featureId: "1" },
-  { sectionId: "B", featureId: "2" },
-  { sectionId: "C", featureId: "3" },
-];
-
 function App() {
+  const [sectionsData, setSectionsData] = useState([]);
   const [message, setMessage] = useState("");
+
+  const fetchSectionsData = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/api/sections");
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      setSectionsData(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchSectionsData();
+  }, []);
 
   const handleSearchClick = (sectionId, featureId) => {
     setMessage(
@@ -81,16 +38,13 @@ function App() {
   return (
     <div className="app">
       <Navbar />
-      {/* {sectionsData.map((section) => ( */}
+
       <Section
-        // key={section.sectionId}
-        // sectionId={section.sectionId}
-        // featureId={section.featureId}
         sectionsData={sectionsData}
         onSearchClick={handleSearchClick}
         onAddClick={handleAddClick}
       />
-      {/* ))} */}
+
       <DisplayResult message={message} />
     </div>
   );
